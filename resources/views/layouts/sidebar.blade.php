3f23a84a0d305c3e7d30cae8fa2e1bdb66d7ba5e
Kresna Satya <satyakresna@unud.ac.id>
2022-12-14 16:16:06 +0800

    add multilevel menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -88,6 +88,47 @@
                     <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
                 </a>
             </li>
+
+            <li class="sidebar-item">
+                <a data-bs-target="#multi" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-corner-right-down align-middle">
+                        <polyline points="10 15 15 20 20 15"></polyline>
+                        <path d="M4 4h7a4 4 0 0 1 4 4v12"></path>
+                    </svg> <span class="align-middle">Multi Level</span>
+                </a>
+                <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar" style="">
+                    <li class="sidebar-item">
+                        <a data-bs-target="#multi-2" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">Two Levels</a>
+                        <ul id="multi-2" class="sidebar-dropdown list-unstyled collapse">
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">Item 1</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">Item 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a data-bs-target="#multi-3" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">Three Levels</a>
+                        <ul id="multi-3" class="sidebar-dropdown list-unstyled collapse">
+                            <li class="sidebar-item">
+                                <a data-bs-target="#multi-3-1" data-bs-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">Item 1</a>
+                                <ul id="multi-3-1" class="sidebar-dropdown list-unstyled collapse">
+                                    <li class="sidebar-item">
+                                        <a class="sidebar-link" href="#">Item 1</a>
+                                    </li>
+                                    <li class="sidebar-item">
+                                        <a class="sidebar-link" href="#">Item 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">Item 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
         </ul>
 
         <div class="sidebar-cta">
