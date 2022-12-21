0fe2d473d076e749d741a09cf779e53254dc2370
Kresna Satya <satyakresna@unud.ac.id>
2022-12-21 15:06:39 +0800

    replace placeholder name with username

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -143,7 +143,7 @@
                 </a>
 
                 <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                    <img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
+                    <img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="{{ auth('imissu-web')->user()->preferred_username }}" /> <span class="text-dark">{{ auth('imissu-web')->user()->preferred_username }}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end">
                     <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
@@ -152,7 +152,7 @@
                     <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
                     <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Log out</a>
+                    <a class="dropdown-item" href="{{ route('sso.web.logout') }}">Log out</a>
                 </div>
             </li>
         </ul>
