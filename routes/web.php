c4604ff1a8f9ca91697edf94ef782411733c0a48
Kresna Satya <satyakresna@unud.ac.id>
2023-01-06 15:12:40 +0800

    protect route with middleware

--- routes/web.php
+++ routes/web.php
@@ -35,20 +35,21 @@ Route::middleware(['imissu-web'])->group(function () {
     Route::get('/', function () {
         return view('blank');
     });
-    Route::get('/menus', [MenuController::class, 'index'])->name('menus.index');
-    Route::post('/menus/refresh', [MenuController::class, 'refresh'])->name('menus.refresh');
-    Route::post('/menus/delete', [MenuController::class, 'delete'])->name('menus.delete');
-    Route::get('/menus/create', MenuCreate::class)->name('menus.create');
-    Route::get('/menus/{id}/edit', MenuEdit::class)->name('menus.edit');
     
-    Route::get('/rbac/users', RBACUsersIndex::class)->name('rbac.users.index');
-    Route::get('/rbac/users/{username}/assign', RBACUsersAssign::class)->name('rbac.users.assign');
+    Route::get('/menus', [MenuController::class, 'index'])->name('menus.index')->middleware('imissu-web.permission:menus.index');
+    Route::post('/menus/refresh', [MenuController::class, 'refresh'])->name('menus.refresh')->middleware('imissu-web.permission:menus.edit');
+    Route::post('/menus/delete', [MenuController::class, 'delete'])->name('menus.delete')->middleware('imissu-web.permission:menus.delete');
+    Route::get('/menus/create', MenuCreate::class)->name('menus.create')->middleware('imissu-web.permission:menus.create');
+    Route::get('/menus/{id}/edit', MenuEdit::class)->name('menus.edit')->middleware('imissu-web.permission:menus.edit');
+    
+    Route::get('/rbac/users', RBACUsersIndex::class)->name('rbac.users.index')->middleware('imissu-web.permission:rbac.users.index');
+    Route::get('/rbac/users/{username}/assign', RBACUsersAssign::class)->name('rbac.users.assign')->middleware('imissu-web.permission:rbac.users.edit');
 
-    Route::get('/rbac/roles', RBACRolesIndex::class)->name('rbac.roles.index');
-    Route::get('/rbac/roles/create', RBACRolesCreate::class)->name('rbac.roles.create');
-    Route::get('/rbac/roles/{role}/edit', RBACRolesEdit::class)->name('rbac.roles.edit');
+    Route::get('/rbac/roles', RBACRolesIndex::class)->name('rbac.roles.index')->middleware('imissu-web.permission:rbac.roles.edit');
+    Route::get('/rbac/roles/create', RBACRolesCreate::class)->name('rbac.roles.create')->middleware('imissu-web.permission:rbac.roles.edit');
+    Route::get('/rbac/roles/{role}/edit', RBACRolesEdit::class)->name('rbac.roles.edit')->middleware('imissu-web.permission:rbac.roles.edit');
 
-    Route::get('/rbac/permissions', RBACPermissionsIndex::class)->name('rbac.permissions.index');
-    Route::get('/rbac/permissions/create', RBACPermissionsCreate::class)->name('rbac.permissions.create');
-    Route::get('/rbac/permissions/{permission}/edit', RBACPermissionsEdit::class)->name('rbac.permissions.edit');
+    Route::get('/rbac/permissions', RBACPermissionsIndex::class)->name('rbac.permissions.index')->middleware('imissu-web.permission:rbac.permissions.edit');
+    Route::get('/rbac/permissions/create', RBACPermissionsCreate::class)->name('rbac.permissions.create')->middleware('imissu-web.permission:rbac.permissions.create');
+    Route::get('/rbac/permissions/{permission}/edit', RBACPermissionsEdit::class)->name('rbac.permissions.edit')->middleware('imissu-web.permission:rbac.permissions.edit');
 });
