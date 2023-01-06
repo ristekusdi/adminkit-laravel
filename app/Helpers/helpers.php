1d53aa1a04b41319bca4c963e909399af112b35f
Kresna Satya <satyakresna@unud.ac.id>
2023-01-06 14:28:57 +0800

    create utils helper (array column recursive)

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,4 +1,5 @@
 <?php
 
+require_once 'utils.php';
 require_once 'app_menu.php';
 require_once 'sortable_menu.php';
---
+++ app/Helpers/utils.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Returns values for $needle key in a multidimensional array, recursively.
+ * More info and example: https://github.com/NinoSkopac/array_column_recursive
+ *
+ * @param array $haystack
+ * @param string $needle
+ * @return array
+ */
+if (! function_exists('array_column_recursive')) {
+    function array_column_recursive(array $haystack, $needle) {
+        $found = [];
+        array_walk_recursive($haystack, function($value, $key) use (&$found, $needle) {
+            if ($key == $needle)
+                $found[] = $value;
+        });
+    
+        return $found;
+    }
+}
